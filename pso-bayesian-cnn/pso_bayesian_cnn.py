eb0f6b6170d748548bde5cbd74b91c0b7e19ddb4
Animesh Tiwari <42550496+animeshrdso@users.noreply.github.com>
2020-10-19 10:26:43 +0530

    Update pso_bayesian_cnn.py

--- pso-bayesian-cnn/pso_bayesian_cnn.py
+++ pso-bayesian-cnn/pso_bayesian_cnn.py
@@ -71,6 +71,7 @@ class Model(nn.Module):
         x = torch.flatten(x, 1)
         x = F.relu(x)
         x = self.fc1(x)
+        x = self.softmax(x)
         return x
 
     # Used to apply softmax and obtain loss value
@@ -88,7 +89,7 @@ class Model(nn.Module):
             _, predicted = torch.max(a.data, 1)
             y_pred[i] = predicted
             b = copy.deepcopy(a)
-            prob[i] = self.softmax(b)
+            prob[i] = b
             loss = self.criterion(a, labels)
             self.los += loss
         
